e81eabe01686378a89fdbc986611bfa4245a23f9
Vladislav <nerokvent7@gmail.com>
2024-02-28 20:23:12 +0300

    https://datahacker.rs/005-how-to-create-a-panorama-image-using-opencv-with-python/

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+import cv2
+import numpy as np
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+img1 = cv2.imread('2024-02-28-185947.jpg')
+img2 = cv2.imread('2024-02-28-185947.jpg')
+
+img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('img1', img1_gray)
+cv2.imshow('img2', img2_gray)
+
+orb = cv2.ORB_create(nfeatures=2000)
+
+# Find the key points and descriptors with ORB
+keypoints1, descriptors1 = orb.detectAndCompute(img1, None)
+keypoints2, descriptors2 = orb.detectAndCompute(img2, None)
+
